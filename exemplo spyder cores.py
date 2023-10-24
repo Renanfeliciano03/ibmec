da42b4ec78f1b5c78d2e8d33f4d435d39d3826b3
Renanfeliciano03 <141173909+Renanfeliciano03@users.noreply.github.com>
2023-10-24 14:41:43 -0300

    Add files via upload

---
+++ exemplo spyder cores.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 14:15:28 2023
+
+@author: 202302790496
+"""
+
+def simples(azul):
+    if cor == 'azul':
+      return 'escolheu certo'
+def medio(azul):
+      if cor =='azul':
+          return 'escolheu certo'
+      else:
+          return 'tente outra cor'
+    
+    
+def completo(azul):
+    if cor =='azul':
+         return 'escolheu certo'
+    elif cor == 'marrom':
+         return 'não tem salvação'
+    else:
+          return 'tente outra cor'
+      
+     
+        
+      
+
+
